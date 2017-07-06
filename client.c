e711e9da4d361f427185b2672976a75e59e7cb0e
codding-hu <codding@sugrsugr.com>
2017-07-06 14:46:00 +0800

    服务器和客户端框架 -----第二版

--- client.c
+++ client.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
-#include <stdlib.h>
-#define  MAX_BACKLOG 20
-int status;
-int s,s_bind;
-struct addrinfo hints;
-struct addrinfo *servinfo;
-struct sockaddr_storage their_addr;
-char buf[100];
-int rev_buf(int s,char*buf);
-int client_init(char*addr,char*port);
-int client_exit();
+#include "client.h"
 int client_init(char*addr,char*port)
 {
    memset(&hints,0,sizeof(hints));
